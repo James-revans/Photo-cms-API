55b6081373f9e1e55319ed6be0bce0f6be20c577
James <evans.james00@gmail.com>
2019-10-19 00:11:14 -0700

    Server error fixing

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ require('./controllers/auth');
 
 var app = express();
 // app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
-app.use(cors({credentials: true, origin: '*'}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
